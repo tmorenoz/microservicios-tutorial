d9350ca5a34a99d2ca625e620db19fde83d95baf
tmoreno <tmoreno@canvia.com>
2023-12-03 23:54:18 -0500

    config server

--- com.config.service/com.config.service/src/main/java/com/config/service/ConfigServiceApplication.java
+++ com.config.service/com.config.service/src/main/java/com/config/service/ConfigServiceApplication.java
@@ -2,13 +2,14 @@ package com.config.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConFigServer
-public class Application {
+@EnableConfigServer
+public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
 
 }
